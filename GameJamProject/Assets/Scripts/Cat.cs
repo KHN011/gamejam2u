3c34818c810dbde91ad1c55c1d8cf8e9dcd57248
KHN011 <hartmann.kristof92@gmail.com>
2023-06-15 15:24:39 +0200

    Main scene visual update

--- GameJamProject/Assets/Scripts/Cat.cs
+++ GameJamProject/Assets/Scripts/Cat.cs
@@ -1,15 +1,57 @@
 
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+using System.Collections;
+using System.Threading;
+
 public class Cat : Target
 {
+    [SerializeField] float speed;
+    bool enableRandomization = false;
+    bool facingLeft;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //const float waitTime = 2.0f;
+        //StartCoroutine(WaitAndPrint(waitTime));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        if (enableRandomization)
+        {            
+            RandomizeMovement();
+        }
         
+    }
+
+    public void onShooted()
+    {
+        // play sound
+        // play anim ?
+        // set score
+        Destroy(gameObject);
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        enableRandomization = true;
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.left * speed * Time.deltaTime;
+    }
+
+    void RandomizeMovement()
+    {
+
     }
 }
