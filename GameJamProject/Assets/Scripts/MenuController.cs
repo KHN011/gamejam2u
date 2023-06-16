1ad891db0c282cd2676fc6ca570fd1d98c13fa48
KHN011 <hartmann.kristof92@gmail.com>
2023-06-16 10:19:06 +0200

    Instructions menu added

--- GameJamProject/Assets/Scripts/MenuController.cs
+++ GameJamProject/Assets/Scripts/MenuController.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    public Button playBtn;
+    public Button howtoplayBtn;
+    public Button exitBtn;
+    public Button backBtn;
+    public GameObject instructions;
+
+    private void Start()
+    {
+        backBtn.gameObject.SetActive(false);
+        instructions.SetActive(false);
+    }
+
     public void onPlay()
     {
         SceneManager.LoadScene("Main");
@@ -13,5 +26,24 @@ public class MenuController : MonoBehaviour
     public void onExit()
     {
         Application.Quit();
+    }
+
+    public void onHowtoplay()
+    {
+
+        playBtn.gameObject.SetActive(false);
+        howtoplayBtn.gameObject.SetActive(false);
+        exitBtn.gameObject.SetActive(false);
+        backBtn.gameObject.SetActive(true);
+        instructions.SetActive(true);
+    }
+
+    public void onBack()
+    {
+        playBtn.gameObject.SetActive(true);
+        howtoplayBtn.gameObject.SetActive(true);
+        exitBtn.gameObject.SetActive(true);
+        backBtn.gameObject.SetActive(false);
+        instructions.SetActive(false);
     }
 }
